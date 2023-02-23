249b7f2ed70559cd048cb77107b05fbdf79692f9
Francsy <francsyludens@gmail.com>
2023-02-23 18:07:58 +0100

    add / logic home

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React, { useContext }from "react";
 import { pokemonsContext } from "../../../context/pokemonsContext";
 import List from "../Search/List";
+import { Link } from 'react-router-dom'
 const Home = () => {
   const { pokemons } = useContext(pokemonsContext)
 
 
   return <section>
     <h1>Your Pokemon List</h1>
-    {pokemons.length > 0 ? <List pokemons={pokemons} /> : <></>}
+    {pokemons.length > 0 ? <List pokemons={pokemons} /> : 
+    <><p>You didn´t add any Pokemon</p><Link to={'/search'}><button>Get them all!</button></Link><p>or</p>
+    <Link to={'/new'}><button>Create yours!</button></Link></>}
   </section>;
 };
 
