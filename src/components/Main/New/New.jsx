e4618cb9c5d8eb90f41772b9257052d8e058c567
Francsy <francsyludens@gmail.com>
2023-02-23 01:14:36 +0100

    add/form with react-hook-form library

--- src/components/Main/New/New.jsx
+++ src/components/Main/New/New.jsx
@@ -1,9 +1,83 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { pokemonsContext } from "../../../context/pokemonsContext";
+import { useForm } from 'react-hook-form'
 
 const New = () => {
-  const { pokemons, setNewPokemon } = useContext(pokemonsContext)
-  return <div>New</div>;
+  const { setNewPokemon } = useContext(pokemonsContext)
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [message, setMessage] = useState(false)
+
+  const onSubmit = data => {
+    const newPokemon = {
+      ...data,
+      name: data.name.toLowerCase()
+    }
+    setNewPokemon(newPokemon)
+    setMessage(true)
+  }
+
+  return (
+    <section>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      {errors.id && <span className="error-message">Must be greater than 0:</span>}
+      <input type="number" placeholder="ID" {...register("id", { required: true, min: 0 })} className={errors.id ? "error-input" : ""} />
+      {errors.name && <span className="error-message">Required and should have more than 3 characters:</span>}
+      <input type="text" placeholder="name" {...register("name", { required: true, minLength: 3 })} className={errors.name ? "error-input" : ""} />
+      {errors.image && <span className="error-message">Required:</span>}
+      <input type="url" placeholder="image" {...register("image", { required: true })} className={errors.image ? "error-input" : ""} />
+      {errors.typeOne && <span className="error-message">First type is required:</span>}
+      <select {...register("typeOne", { required: true })} className={errors.typeOne ? "error-input" : ""} defaultValue="" >
+        <option value="" disabled>First Type</option>
+        <option value="normal">Normal</option>
+        <option value="fighting">Fighting</option>
+        <option value="flying">Flying</option>
+        <option value="poison">Poison</option>
+        <option value="ground">Ground</option>
+        <option value="bug">Bug</option>
+        <option value="ghost">Ghost</option>
+        <option value="fire">Fire</option>
+        <option value="water">Water</option>
+        <option value="grass">Grass</option>
+        <option value="electric">Electric</option>
+        <option value="psychic">Psychic</option>
+        <option value="ice">Ice</option>
+        <option value="dragon">Dragon</option>
+        <option value="dark">Dark</option>
+        <option value="Fairy">Fairy</option>
+        <option value="Unknown">Unknown</option>
+        <option value="Developer">Developer</option>
+      </select>
+      <select {...register("typeTwo")} defaultValue="">
+        <option value="" disabled >Second Type</option>
+        <option value="normal">Normal</option>
+        <option value="fighting">Fighting</option>
+        <option value="flying">Flying</option>
+        <option value="poison">Poison</option>
+        <option value="ground">Ground</option>
+        <option value="bug">Bug</option>
+        <option value="ghost">Ghost</option>
+        <option value="fire">Fire</option>
+        <option value="water">Water</option>
+        <option value="grass">Grass</option>
+        <option value="electric">Electric</option>
+        <option value="psychic">Psychic</option>
+        <option value="ice">Ice</option>
+        <option value="dragon">Dragon</option>
+        <option value="dark">Dark</option>
+        <option value="Fairy">Fairy</option>
+        <option value="Unknown">Unknown</option>
+        <option value="Developer">Developer</option>
+      </select>
+      <input type="submit" />
+    </form>
+    {message ? <article> 
+      <p><strong>¡¡Pokemon added!!</strong></p><img src="/assets/pokemon-created.gif" alt="Pokemon created" />
+    </article>
+     : <></>}
+    </section>
+    
+  );
+
 };
 
 export default New;
