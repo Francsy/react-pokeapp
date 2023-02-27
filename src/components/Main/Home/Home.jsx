68b70cd1b841ff64bfc6f5819a4b105a1a898728
Francsy <francsyludens@gmail.com>
2023-02-27 21:03:55 +0100

    add / form styles

--- src/components/Main/Home/Home.jsx
+++ src/components/Main/Home/Home.jsx
@@ -1,4 +1,5 @@
-import React, { useContext }from "react";
+import React, { useContext, useState, useEffect }from "react";
+
 import { pokemonsContext } from "../../../context/pokemonsContext";
 import List from "../Search/List";
 import { Link } from 'react-router-dom'
@@ -8,7 +9,21 @@ import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   const { pokemons, deletePokemon } = useContext(pokemonsContext)
+  const [buttonSize, setButtonSize] = useState('')
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 768) {
+        setButtonSize("lg");
+      } else {
+        setButtonSize("");
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return <section className="home">
     <img className="home-title-pokemon" src="/assets/pokemon-title-color.png" alt="" />
@@ -16,14 +31,12 @@ const Home = () => {
     <><article className="home-start-article"><h3>You didn´t add any Pokemon yet!!</h3><div>
 
       <Link to={'/search'}>
-      <Button className="home-left-button" variant="dark">Get them all!</Button>
+      <Button className="home-left-button" size={buttonSize} variant="dark">Get them all!</Button>
       </Link>
       <Link to={'/new'}>
-      <Button className="home-right-button" variant="dark">Create Yours!</Button>
+      <Button className="home-right-button" size={buttonSize} variant="dark">Create Yours!</Button>
       </Link>
 
-      
-    
     </div></article></>}
   </section>;
 };
